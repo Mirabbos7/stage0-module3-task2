39e1b584b970beb0df00921fc77cdd6a088d10df
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-04-18 21:15:51 +0500

    Edited

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -3,10 +3,10 @@ package school.mjc.stage0.module3.task2;
 public class AdvancedNamingConvention {
     private static final int ADULT_AGE = 18;
 
-    private int AGE;
+    private int Age;
     private int phoneNumber;
 
-    void CAllToFriend() {
+    void callToFriend() {
         //some code
     }
 
